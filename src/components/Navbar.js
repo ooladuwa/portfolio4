e2cc3a6917b2921c0eecbcbaaccd3a141799e278
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-18 09:32:03 -0400

    have navbar set. working on getting homepage background image set

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,13 @@ import "./navbar.css"
 
 const Navbar = () => {
   return (
-    <nav className="navbar-container">
-      <Link className="link" to="/">Home  |</Link>
-      <Link className="link" to="/about">About  |</Link>
-      <Link className="link" to="projects">Projects</Link>
-    </nav>
+    <header>
+      <nav className="navbar-container">
+        <Link className="link" to="/">Home  |</Link>
+        <Link className="link" to="/about">About  |</Link>
+        <Link className="link" to="projects">Projects</Link>
+      </nav>
+    </header>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import "./home.css"
 const Home = () => {
   return (
     <div className="home-container">
-      HOME
+     
     </div>
   )
 }
